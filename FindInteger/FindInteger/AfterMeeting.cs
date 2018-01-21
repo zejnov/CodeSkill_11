1f5245f7312b918e296d63581f00ec6386ba8649
Mati ZejnoV <mati.szwaba@gmail.com>
2018-01-21 13:09:56 +0100

    working on extensions methods

--- FindInteger/FindInteger/AfterMeeting.cs
+++ FindInteger/FindInteger/AfterMeeting.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using System.Linq;
 
 namespace FindInteger
 {
+    //input: oredered list of integeres, ASC & desc(later).
+
     class AfterMeeting
     {
         public void Run(int[] givenList)
@@ -15,11 +17,35 @@ namespace FindInteger
             var length = givenList.Length;
 
             //code goes there
-            
+            givenList.IsNegative();
+
             FindInteger(givenList);
 
 
             return null;
+        }
+    }
+
+    public static class MyExtensions
+    {
+        public static bool IsNegative(this int[] array)
+        {
+            return array.First() < 0 && array.Last() < 0;
+        }
+
+        public static bool IsPositive(this int[] array)
+        {
+            return array.First() >= 0 && array.Last() > 0;
+        }
+
+        public static bool IsTroughZero(this int[] array)
+        {
+            return array.First() < 0 && array.Last() > 0;
+        }
+
+        public static bool HasSameValues(this int[] array)
+        {
+            return array.First() == array.Last();
         }
     }
 }
