faa26c264716eb44048706a8595c79ede239b84f
Mati ZejnoV <mati.szwaba@gmail.com>
2018-01-22 19:21:26 +0100

    on dev branch

---
+++ FindInteger/FindInteger/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace FindInteger
+{
+    public class Program
+    {
+        public AfterMeeting IntSearcher = new AfterMeeting();
+
+        public static void Main()
+        {
+            new Program().Execute();
+        }
+
+        private void Execute()
+        {
+            var givenList = new[] { -11, -2, 0, 0, 2 };
+
+            var sth = IntSearcher.Run(givenList);
+
+            Console.WriteLine(sth == null ? $"null" : $"{sth.Value}");
+            Console.ReadLine();
+        }
+    }
+}
